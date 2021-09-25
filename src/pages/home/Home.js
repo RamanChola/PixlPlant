7eb859e8ae9d6bbf0c46ff1d82bc8579c3defcdf
RexRose <bikeedwin@gmail.com>
2021-09-25 14:36:33 -0400

    Added buttons for adding and deleting plants. Added buttons to be covered by plants to lead to care information.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,66 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../Auth/authContext";
 import Navbar from "../../components/Navbar";
+import { Button } from "@mui/material";
 
 const Home = () => {
   const auth = useContext(AuthContext);
   return (
     <div>
       <Navbar />
-      Welcome to homepage, {auth.user}
+      <b>Welcome to homepage, {auth.user} </b>
+      <Button style={{
+            position: "absolute",
+            left: "auto",
+            marginLeft: "auto",
+            marginTop: "50px",
+            marginRight: "220px",
+            textAlign: "center",
+            backgroundColor: "transparent",
+            color:"black"
+          }}
+          variant="outlined" size="large">
+        Add Plant
+      </Button>
 
+      <Button style={{
+            position: "absolute",
+            right: "auto",
+            marginLeft: "220px",
+            marginTop: "50px",
+            marginRight: "auto",
+            textAlign: "center",
+            backgroundColor: "transparent",
+            color:"black"
+          }}
+          variant="outlined" size="large">
+        Remove Plant
+      </Button>
+      
+      <Button style={{
+            position: "absolute",
+            marginLeft: "600px",
+            marginTop: "400px",
+            textAlign: "center",
+            backgroundColor: "transparent",
+            color:"black"
+          }}
+          variant="outlined" size="large">
+        Plant 1
+      </Button>
+
+      <Button style={{
+            position: "absolute",
+            marginLeft: "800px",
+            marginTop: "400px",
+            textAlign: "center",
+            backgroundColor: "transparent",
+            color:"black"
+          }}
+          variant="outlined" size="large">
+        Plant 2
+      </Button>
+      
     </div>
   );
 };
