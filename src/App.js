7e6e5f93017e757fd801a683e9bd17a714ec0ed1
RexRose <bikeedwin@gmail.com>
2021-09-25 03:35:03 -0400

    Input fields for username and password. Background color gradient.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      hi hello
-      allo
-      Hello World
-    </div>
+import React from "react";
+import { useForm } from "react-hook-form";
+import './App.css';
+
+export default function App() {
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return(
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="username" {...register('Username', { required: true })} />
+      <input type="password" {...register('Password', { required: true })} />
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
